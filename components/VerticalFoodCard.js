478ce06a088d1b309087b35401a5bb345deec355
Rehan41302 <saeedrehan3@gmail.com>
2023-03-01 23:38:30 +0500

    feat[home]: added category section with filters

--- components/VerticalFoodCard.js
+++ components/VerticalFoodCard.js
@@ -15,39 +15,47 @@ const VerticalFoodCard = ({ containerStyle, item, onPress }) => {
         ...containerStyle,
       }}
     >
+      <View style={{ flexDirection: "row" }}>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <Image source={icons.calories} style={{ width: 30, height: 30 }} />
+          <Text style={{ color: COLORS.darkGray2, ...FONTS.body5 }}>
+            {item.calories} Calories
+          </Text>
+        </View>
+        <Image
+          source={icons.love}
+          style={{
+            width: 20,
+            height: 20,
+            tintColor: item.isFavourite ? COLORS.primary : COLORS.gray,
+          }}
+        />
+      </View>
       <View
         style={{
-          flexDirection: "row",
-          position: "absolute",
-          top: 5,
-          right: SIZES.radius,
+          height: 150,
+          width: 150,
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <Image source={icons.calories} style={{ width: 30, height: 30 }} />
-        <Text style={{ color: COLORS.darkGray2, ...FONTS.body5 }}>
-          {item.calories} Calories
-        </Text>
+        <Image
+          source={item.image}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
       </View>
-      <Image
-        source={icons.calories}
-        style={{
-          width: 20,
-          height: 20,
-          tintColor: item.isFavourite ? COLORS.primary : COLORS.gray,
-        }}
-      />
-      <View style={{ height: 150, width: 150, alignItems: 'center', justifyContent: 'center',  }}>
-      <Image
-        source={item.image}
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-      />
-      </View>
-      <View style={{ alignItems: 'center', marginTop: -20 }}>
-        <Text style={{ ...FONTS.h3}}>{item.name}</Text>
-        <Text style={{ color: COLORS.darkGray2, textAlign: 'center', ...FONTS.body5 }}>
+      <View style={{ alignItems: "center", marginTop: -20 }}>
+        <Text style={{ ...FONTS.h3 }}>{item.name}</Text>
+        <Text
+          style={{
+            color: COLORS.darkGray2,
+            textAlign: "center",
+            ...FONTS.body5,
+          }}
+        >
           {item.description}
         </Text>
         <Text style={{ marginTop: SIZES.base, ...FONTS.h2 }}>
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -212,6 +212,57 @@ const Home = () => {
     );
   }
 
+  function rendeFoodCategorySection() {
+    return (
+      <FlatList
+        data={dummyData.categories}
+        keyExtractor={(item) => `${item.id}`}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              height: 55,
+              marginTop: SIZES.padding,
+              marginLeft: index === 0 ? SIZES.padding : SIZES.radius,
+              marginRight:
+                index === dummyData.categories.length - 1 ? SIZES.padding : 0,
+              paddingHorizontal: 8,
+              borderRadius: SIZES.radius,
+              backgroundColor:
+                selectedCategoryId === item.id
+                  ? COLORS.primary
+                  : COLORS.lightGray2,
+            }}
+            onPress={() => {
+              setSelectedCategoryId(item.id);
+              handleChangeCategory(item.id, selectedMenuType);
+            }}
+          >
+            <Image
+              source={item.icon}
+              style={{ marginTop: 5, height: 50, width: 50 }}
+            />
+            <Text
+              style={{
+                alignSelf: "center",
+                marginRight: SIZES.base,
+                color:
+                  item.id === selectedCategoryId
+                    ? COLORS.white
+                    : COLORS.darkGray,
+                ...FONTS.h3,
+              }}
+            >
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    );
+  }
+
   return (
     // Tackle the bottom tabs height with marginBottom
     <View style={{ flex: 1, marginBottom: 140 }}>
@@ -225,6 +276,7 @@ const Home = () => {
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={
           <View>
+            {rendeFoodCategorySection()}
             {renderPopularSection()}
             {renderRecommendedSection()}
             {renderMenuTypes()}
