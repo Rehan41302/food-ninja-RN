7c23957689eeaa1e78fa2eb8cc3ff42bd9a409fe
Rehan41302 <saeedrehan3@gmail.com>
2023-03-03 23:47:14 +0500

    setting up build vonfig

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -11,6 +11,7 @@ import {
 import HorizintalFoodCard from "../../components/HorizintalFoodCard";
 import VerticalFoodCard from "../../components/VerticalFoodCard";
 import { FONTS, SIZES, COLORS, icons, dummyData } from "../../constants";
+import FilterModal from "./FilterModal";
 
 const Section = ({ title, onPress, children }) => {
   return (
@@ -39,9 +40,11 @@ const Section = ({ title, onPress, children }) => {
 const Home = () => {
   const [selectedCategoryId, setSelectedCategoryId] = useState(1);
   const [selectedMenuType, setSelectedMenuType] = useState(1);
-  const [menuList, setMenuList] = useState([]);
-  const [recommends, setRecommends] = useState([]);
   const [popular, setPopular] = useState([]);
+  const [recommends, setRecommends] = useState([]);
+  const [menuList, setMenuList] = useState([]);
+
+  const [showFilterModal, setShowFilterModal] = useState(false);
 
   useEffect(() => {
     handleChangeCategory(selectedCategoryId, selectedMenuType);
@@ -101,9 +104,7 @@ const Home = () => {
           style={{ flex: 1, marginLeft: SIZES.radius, ...FONTS.body3 }}
           placeholder="Search food..."
         />
-        <TouchableOpacity
-        // onPress={}
-        >
+        <TouchableOpacity onPress={() => setShowFilterModal(true)}>
           <Image
             source={icons.filter}
             style={{ height: 20, width: 20, tintColor: COLORS.black }}
@@ -290,9 +291,17 @@ const Home = () => {
 
   return (
     // Tackle the bottom tabs height with marginBottom
-    <View style={{ flex: 1  }}>
+    <View style={{ flex: 1 }}>
       {/* Search */}
       {renderSearch()}
+
+      {/* Filter */}
+      {showFilterModal && (
+        <FilterModal
+          isVisible={showFilterModal}
+          onClose={() => setShowFilterModal(false)}
+        />
+      )}
 
       {/* List */}
       <FlatList
