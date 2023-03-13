d8e96afe2568060853af8fdcd090498f21c7374e
Rehan41302 <saeedrehan3@gmail.com>
2023-03-13 23:13:21 +0500

    chore: adding new screens routes

--- App.js
+++ App.js
@@ -33,11 +33,28 @@ const App = () => {
 
           <Stack.Screen name="SignIn" component={SignIn} />
 
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          
           <Stack.Screen name="SignUp" component={SignUp} />
 
-          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-
           <Stack.Screen name="Otp" component={Otp} />
+
+          <Stack.Screen name="FoodDetail" component={FoodDetail} />
+
+          <Stack.Screen name="Checkout" component={Checkout} />
+
+          <Stack.Screen name="MyCart" component={MyCart} />
+
+          <Stack.Screen name="Success" component={Success} />
+
+          <Stack.Screen name="AddCard" component={AddCard} />
+
+          <Stack.Screen name="MyCard" component={MyCard} />
+
+          <Stack.Screen name="DeliveryStatus" component={DeliveryStatus} />
+
+          <Stack.Screen name="Map" component={Map} />
+
           <Stack.Screen name="Home" component={CustomDrawer} />
         </Stack.Navigator>
       </NavigationContainer>
