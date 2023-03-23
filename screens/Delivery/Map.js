250590d1ef4a35bb0f3cadd92bda4417e1125ee5
Rehan41302 <saeedrehan3@gmail.com>
2023-03-23 16:31:41 +0500

    feat: completed map screen

--- screens/Delivery/Map.js
+++ screens/Delivery/Map.js
@@ -34,7 +34,7 @@ const Map = ({ navigation }) => {
   const [angle, setAngle] = useState(0);
 
   const [isReady, setIsReady] = useState(false);
-  const [duration, setDuration] = useState("");
+  const [duration, setDuration] = useState("30");
 
   useEffect(() => {
     let initialRegion = {
@@ -118,7 +118,167 @@ const Map = ({ navigation }) => {
     );
   }
 
-  return <View style={{ flex: 1 }}>{renderMap()}</View>;
+  function renderHeaderButtons() {
+    return (
+      <>
+        <IconButton
+          icon={icons.back}
+          containerStyle={{
+            position: "absolute",
+            top: SIZES.padding * 2,
+            left: SIZES.padding,
+            ...style.buttonStyle,
+          }}
+          iconStyle={{ width: 20, height: 20, tintColor: COLORS.gray2 }}
+          onPress={() => navigation.goBack()}
+        />
+
+        <View
+          style={{
+            position: "absolute",
+            top: SIZES.padding * 2,
+            right: SIZES.padding,
+          }}
+        >
+          <IconButton
+            icon={icons.globe}
+            containerStyle={style.buttonStyle}
+            iconStyle={{ width: 20, height: 20, tintColor: COLORS.gray2 }}
+          />
+          <IconButton
+            icon={icons.focus}
+            containerStyle={{ marginTop: SIZES.radius, ...style.buttonStyle }}
+            iconStyle={{ width: 20, height: 20, tintColor: COLORS.gray2 }}
+          />
+        </View>
+      </>
+    );
+  }
+
+  function renderInfo() {
+    return (
+      <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          colors={[COLORS.transparent, COLORS.lightGray1]}
+          style={{
+            position: "absolute",
+            top: -20,
+            left: 0,
+            right: 0,
+            height: Platform.OS === "ios" ? 200 : 50,
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+          }}
+        />
+
+        <View
+          style={{
+            padding: SIZES.padding,
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+            backgroundColor: COLORS.white,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image
+              source={icons.clock}
+              style={{ width: 40, height: 40, tintColor: COLORS.black }}
+            />
+            <View style={{ marginLeft: SIZES.padding }}>
+              <Text style={{ color: COLORS.gray, ...FONTS.body4 }}>
+                Your delivery time
+              </Text>
+              <Text style={{ ...FONTS.h3 }}>{duration} minutes</Text>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: SIZES.padding,
+            }}
+          >
+            <Image
+              source={icons.focus}
+              style={{ width: 40, height: 40, tintColor: COLORS.black }}
+            />
+            <View style={{ marginLeft: SIZES.padding }}>
+              <Text style={{ color: COLORS.gray, ...FONTS.body4 }}>
+                Your Address
+              </Text>
+              <Text style={{ ...FONTS.h3 }}>88, Jln Padungan, Kuching</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              height: 70,
+              marginTop: SIZES.padding,
+              borderRadius: SIZES.radius,
+              paddingHorizontal: SIZES.radius,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: COLORS.primary,
+            }}
+          >
+            <Image
+              source={images.profile}
+              style={{ width: 40, height: 40, borderRadius: 5 }}
+            />
+
+            <View style={{ flex: 1, marginLeft: SIZES.radius }}>
+              <Text style={{ color: COLORS.white, ...FONTS.h3 }}>
+                MicroDevAssociation
+              </Text>
+              <Text style={{ color: COLORS.white, ...FONTS.body4 }}>
+                Delivery man
+              </Text>
+            </View>
+
+            <View
+              style={{
+                height: 40,
+                width: 40,
+                alignItems: "center",
+                justifyContent: "center",
+                borderWidth: 1,
+                borderRadius: 5,
+                borderColor: COLORS.white,
+                backgroundColor: COLORS.transparentPrimray,
+              }}
+            >
+              <Image source={icons.call} style={{ width: 30, height: 30 }} />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      {renderMap()}
+      {renderHeaderButtons()}
+      {renderInfo()}
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  buttonStyle: {
+    width: 40,
+    height: 40,
+    borderRadius: SIZES.radius,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: COLORS.gray2,
+    backgroundColor: COLORS.white,
+  },
+});
 
 export default Map;
